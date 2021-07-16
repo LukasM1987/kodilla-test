85635857c9b88db65bceebdf59777cac0d9b07c3
Lukasz Marchel <marchel.lukas@gmail.com>
2021-07-16 06:42:31 +0200

    KOD-7.3 Forum user stream after filtering

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,23 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamMain {
 
     public static void main(String[] args) {
-
+        Forum forum = new Forum(20);
+        Map<String, ForumUser> forumUsers = forum.getUsersList()
+                .stream().filter(forumUser -> forumUser.getGender() == 'M')
+                .filter(forumUser -> forumUser.getDateOfBirth().getYear() <= 2001)
+                .filter(forumUser -> forumUser.getNumbersOfPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+        System.out.println("Map size: " + forumUsers.size());
+        forumUsers.entrySet().stream()
+                .map(item -> item.getKey() + " - " + item.getValue())
+                .forEach(System.out::println);
     }
 }
