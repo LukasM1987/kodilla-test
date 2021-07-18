63a604b8c3ab8b7fc6604b209d185dffc078533e
Lukasz Marchel <marchel.lukas@gmail.com>
2021-07-18 19:29:22 +0200

    KOD-7.6 days average

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/portfolio/Board.java
@@ -0,0 +1,38 @@
+package com.kodilla.stream.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Board {
+
+    private final List<TaskList> tasksLists = new ArrayList<>();
+    private final String name;
+
+    public Board(final String name) {
+        this.name = name;
+    }
+
+    public void addTaskList(TaskList taskList) {
+        tasksLists.add(taskList);
+    }
+
+    public boolean removeTaskList(TaskList taskList) {
+        return tasksLists.remove(taskList);
+    }
+
+    public List<TaskList> getTasksLists() {
+        return new ArrayList<>(tasksLists);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "tasks=" + tasksLists +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
