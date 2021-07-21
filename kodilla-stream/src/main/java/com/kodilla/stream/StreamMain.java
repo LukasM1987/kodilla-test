3551aaf904491fb6f0c4c31973e67856c0472a98
LukasM1987 <86426303+LukasM1987@users.noreply.github.com>
2021-07-21 10:50:28 +0200

    KOD-7.3 Forum user stream after filtering (#16)
    * KOD-7.3 Forum user stream after filtering
    
    * KOD-7.3 date of birth filter fixed

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,25 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class StreamMain {
 
     public static void main(String[] args) {
-
+        Forum forum = new Forum(100);
+        Map<String, ForumUser> forumUsers = forum.getUsersList()
+                .stream().filter(forumUser -> forumUser.getGender() == 'M')
+                .filter(forumUser -> ChronoUnit.YEARS.between(forumUser.getDateOfBirth(), LocalDate.now()) >= 20)
+                .filter(forumUser -> forumUser.getNumbersOfPosts() >= 1)
+                .collect(Collectors.toMap(ForumUser::getUserID, forumUser -> forumUser));
+        System.out.println("Map size: " + forumUsers.size());
+        forumUsers.entrySet().stream()
+                .map(item -> item.getKey() + " - " + item.getValue())
+                .forEach(System.out::println);
     }
 }
