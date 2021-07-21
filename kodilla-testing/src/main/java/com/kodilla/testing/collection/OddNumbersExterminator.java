e4eb7def0998b04f9e9ed26a3b4c90e012b45d76
Lukasz Marchel <marchel.lukas@gmail.com>
2021-07-21 17:21:47 +0200

    KOD-6.3 rebase

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -5,14 +5,24 @@ import java.util.List;
 
 public class OddNumbersExterminator {
 
+<<<<<<< HEAD
     private int listNumberSize;
     public OddNumbersExterminator(int listNumberSize) {
         this.listNumberSize = listNumberSize;
+=======
+    private int numbersSize;
+    public OddNumbersExterminator(int numbersSize) {
+        this.numbersSize = numbersSize;
+>>>>>>> 06a5de6 (KOD-6.3 Collection odd numbers test)
     }
 
     public List<Integer> exterminate(List<Integer> numbers) {
         List<Integer> evenNumbers = new ArrayList<>();
+<<<<<<< HEAD
         for (int i = 0; i < listNumberSize; i++) {
+=======
+        for (int i = 0; i < numbersSize; i++) {
+>>>>>>> 06a5de6 (KOD-6.3 Collection odd numbers test)
             double modulo = numbers.get(i) % 2;
             if (modulo == 0) {
                 evenNumbers.add(i);
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,6 +1,10 @@
 package com.kodilla.testing.collection;
 
 import org.junit.jupiter.api.*;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 06a5de6 (KOD-6.3 Collection odd numbers test)
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,19 +21,31 @@ public class CollectionTestSuite {
         System.out.println("Test Case: end");
     }
 
+<<<<<<< HEAD
     @DisplayName("Metoda testOddNumbersExterminatorEmptyList powinna" +
             "zwrócić pusta listę.")
+=======
+    @DisplayName("Metoda testOddNumbersExterminatorEmptyList powinna zwrócić pustą.")
+>>>>>>> 06a5de6 (KOD-6.3 Collection odd numbers test)
 
     @Test
     void testOddNumbersExterminatorEmptyList() {
         List<Integer> numbers = new ArrayList<>();
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator(0);
+<<<<<<< HEAD
         System.out.println("Testuje rozmiar listy: " + oddNumbersExterminator.exterminate(numbers).size());
         Assertions.assertEquals(0, oddNumbersExterminator.exterminate(numbers).size());
     }
 
     @DisplayName("Metoda testOddNumbersExterminatorNormalList powinna" +
             "zwrócić listę z liczbami parzystymi.")
+=======
+        System.out.println("Testuje rozmiar listy: " + oddNumbersExterminator.exterminate(numbers));
+        Assertions.assertEquals(0, oddNumbersExterminator.exterminate(numbers).size());
+    }
+
+    @DisplayName("Metoda testOddNumbersExterminatorNormalList powinna zwrócić listę z liczbami parzystymi.")
+>>>>>>> 06a5de6 (KOD-6.3 Collection odd numbers test)
 
     @Test
     void testOddNumbersExterminatorNormalList() {
@@ -38,7 +54,11 @@ public class CollectionTestSuite {
             numbers.add(i);
         }
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator(50);
+<<<<<<< HEAD
         System.out.println(oddNumbersExterminator.exterminate(numbers));
+=======
+        System.out.println("Testuje rozmiar listy: " + oddNumbersExterminator.exterminate(numbers));
+>>>>>>> 06a5de6 (KOD-6.3 Collection odd numbers test)
         Assertions.assertEquals(25, oddNumbersExterminator.exterminate(numbers).size());
     }
 }
