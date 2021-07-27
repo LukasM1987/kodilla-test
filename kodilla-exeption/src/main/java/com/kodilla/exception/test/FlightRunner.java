3de25d8255c6ae473ee0a7059e27f458c28fc11d
Lukasz Marchel <marchel.lukas@gmail.com>
2021-07-27 19:30:29 +0200

    KOD-8.4 fixed RouteFinder class

--- kodilla-exeption/src/main/java/com/kodilla/exception/test/FlightRunner.java
+++ kodilla-exeption/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -1,25 +1,13 @@
 package com.kodilla.exception.test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class FlightRunner {
 
     public static void main(String[] args) {
-        Flight flight = new Flight("Warszawa", "Berlin");
+        Flight flight = new Flight("Warszawa", "azfa");
         RouteFinder findRoute = new RouteFinder();
-        Map<String, Boolean> routes = new HashMap<>();
-        routes.put("Warszawa", true);
-        routes.put("Modlin", true);
-        routes.put("Lodz", false);
-        routes.put("Barcelona", true);
-        routes.put("Radom", false);
-        routes.put("Lublin", false);
-        routes.put("Berlin", true);
-        routes.put("Olsztyn", false);
-
         try {
-            findRoute.findRoutes(flight, routes);
+            findRoute.findRoutes(flight);
         } catch (RouteNotFoundException e) {
             System.out.println("This Airport does not exist. Error: " + e);
         } finally {
--- kodilla-exeption/src/main/java/com/kodilla/exception/test/RouteFinder.java
+++ kodilla-exeption/src/main/java/com/kodilla/exception/test/RouteFinder.java
@@ -1,17 +1,25 @@
 package com.kodilla.exception.test;
 
-import com.kodilla.exception.io.FileReaderException;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class RouteFinder {
 
-    public void findRoutes(Flight flight, Map<String, Boolean> routes) throws RouteNotFoundException {
-        if (!routes.containsKey(flight.getArrivalAirport()) || !routes.containsKey(flight.getDepartureAirptort())) {
+    private static final Map<String, Boolean> routes = new HashMap<>();
+
+    public void findRoutes(Flight flight) throws RouteNotFoundException {
+        routes.put("Warszawa", true);
+        routes.put("Modlin", true);
+        routes.put("Lodz", false);
+        routes.put("Barcelona", true);
+        routes.put("Radom", false);
+        routes.put("Lublin", false);
+        routes.put("Berlin", true);
+        routes.put("Olsztyn", false);
+        if (!routes.containsKey(flight.getArrivalAirport())) {
             throw new RouteNotFoundException();
         } else {
-            if (routes.get(flight.getDepartureAirptort()) && routes.get(flight.getArrivalAirport())) {
+            if (routes.get(flight.getArrivalAirport())) {
                 System.out.println("Connection is existing.");
             } else {
                 System.out.println("Connection does not exist.");
