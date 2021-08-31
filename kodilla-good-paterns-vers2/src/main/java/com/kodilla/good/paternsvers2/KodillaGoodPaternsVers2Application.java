88c38273fa165d7eff7dd7150602071220bcfdfe
Lukasz Marchel <marchel.lukas@gmail.com>
2021-08-31 17:23:35 +0200

    KOD-14.1 forum user task

---
+++ kodilla-good-paterns-vers2/src/main/java/com/kodilla/good/paternsvers2/KodillaGoodPaternsVers2Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.good.paternsvers2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaGoodPaternsVers2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaGoodPaternsVers2Application.class, args);
+    }
+
+}
---
+++ kodilla-spring-intro/src/main/java/com/kodillas/pring/intro/shape/Shape.java
@@ -0,0 +1,6 @@
+package com.kodillas.pring.intro.shape;
+
+public interface Shape {
+
+    String draw();
+}
