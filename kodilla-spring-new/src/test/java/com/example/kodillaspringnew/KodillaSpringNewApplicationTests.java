71df3d8feb51e3c94153509ae0e7e16cec717b5d
Lukasz Marchel <marchel.lukas@gmail.com>
2021-09-15 17:45:56 +0200

    fixing

---
+++ kodilla-spring-new/src/test/java/com/example/kodillaspringnew/KodillaSpringNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillaspringnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
