1114337cdf4621479214cee993dac1564ed835c6
Lukasz Marchel <marchel.lukas@gmail.com>
2021-10-09 14:51:45 +0200

    KOD-17.3 Task 17.3 with examples

--- kodilla-hibernate/src/main/java/com/kodilla/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/kodilla/hibernate/tasklist/TaskList.java
@@ -1,8 +1,11 @@
 package com.kodilla.kodilla.hibernate.tasklist;
 
+import com.kodilla.kodilla.hibernate.task.Task;
 import com.sun.istack.NotNull;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKSLISTS")
@@ -11,6 +14,7 @@ public class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
 
@@ -37,6 +41,20 @@ public class TaskList {
     @Column(name = "DESCRIPTION")
     public String getDescription() {
         return description;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     private void setListName(String listName) {
