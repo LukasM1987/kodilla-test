ef8a3eb44978a37fd8f738e4deba2934328db587
Lukasz Marchel <marchel.lukas@gmail.com>
2021-10-29 11:13:51 +0200

    KOD-13.5 flights new version

--- kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/flights/FlightRepository.java
+++ kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/flights/FlightRepository.java
@@ -1,22 +1,2 @@
-package com.kodilla.good.paterns.flights;
-
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
-public class FlightRepository {
-
-    private static final Set<Flight> flights = new HashSet<>();
-
-    public Set<Flight> flights() {
-        flights.add(new Flight("Warsaw", "Paris", LocalDate.now()));
-        flights.add(new Flight("Warsaw", "Berlin", LocalDate.now()));
-        flights.add(new Flight("Warsaw", "New York", LocalDate.now()));
-        flights.add(new Flight("Warsaw", "London", LocalDate.now()));
-        flights.add(new Flight("Paris", "Barcelona", LocalDate.now()));
-        flights.add(new Flight("Berlin", "Rome", LocalDate.now()));
-        flights.add(new Flight("Rome", "Miami", LocalDate.now()));
-        flights.add(new Flight("Paris", "Miami", LocalDate.now()));
-        return flights;
-    }
+package com.kodilla.good.paterns.flights;public class FlightRepository {
 }
---
+++ kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/flights/Searcher.java
@@ -0,0 +1,2 @@
+package com.kodilla.good.paterns.flights;public class Searcher {
+}
